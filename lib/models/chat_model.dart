7ad695356fcde4dd971522036fc84a126fc267d9
Eric Batista <klose.eric31@gmail.com>
2020-03-16 21:12:58 -0300

    Done chat model

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,59 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+List<ChatModel> dummyData = [
+  new ChatModel(
+    name: "Eric Batista",
+    message: "Boa noite rapaz!! Bao",
+    time: "15:30",
+    avatarUrl: "https://scontent-gru2-2.xx.fbcdn.net/v/t1.0-9/53860949_1928452457264330_8480711902920441856_n.jpg?_nc_cat=106&_nc_sid=09cbfe&_nc_ohc=I__JRhz-_a4AX9hrtFJ&_nc_ht=scontent-gru2-2.xx&oh=862d42a1e52703ea53331311c86d86fb&oe=5E975F05",
+  ),
+  new ChatModel(
+    name: "Eric Batista",
+    message: "Boa noite rapaz!! Bao",
+    time: "15:30",
+    avatarUrl: "https://scontent-gru2-2.xx.fbcdn.net/v/t1.0-9/53860949_1928452457264330_8480711902920441856_n.jpg?_nc_cat=106&_nc_sid=09cbfe&_nc_ohc=I__JRhz-_a4AX9hrtFJ&_nc_ht=scontent-gru2-2.xx&oh=862d42a1e52703ea53331311c86d86fb&oe=5E975F05",
+  ),
+  new ChatModel(
+    name: "Eric Batista",
+    message: "Boa noite rapaz!! Bao",
+    time: "15:30",
+    avatarUrl: "https://scontent-gru2-2.xx.fbcdn.net/v/t1.0-9/53860949_1928452457264330_8480711902920441856_n.jpg?_nc_cat=106&_nc_sid=09cbfe&_nc_ohc=I__JRhz-_a4AX9hrtFJ&_nc_ht=scontent-gru2-2.xx&oh=862d42a1e52703ea53331311c86d86fb&oe=5E975F05",
+  ),
+  new ChatModel(
+    name: "Eric Batista",
+    message: "Boa noite rapaz!! Bao",
+    time: "15:30",
+    avatarUrl: "https://scontent-gru2-2.xx.fbcdn.net/v/t1.0-9/53860949_1928452457264330_8480711902920441856_n.jpg?_nc_cat=106&_nc_sid=09cbfe&_nc_ohc=I__JRhz-_a4AX9hrtFJ&_nc_ht=scontent-gru2-2.xx&oh=862d42a1e52703ea53331311c86d86fb&oe=5E975F05",
+  ),
+  new ChatModel(
+    name: "Eric Batista",
+    message: "Boa noite rapaz!! Bao",
+    time: "15:30",
+    avatarUrl: "https://scontent-gru2-2.xx.fbcdn.net/v/t1.0-9/53860949_1928452457264330_8480711902920441856_n.jpg?_nc_cat=106&_nc_sid=09cbfe&_nc_ohc=I__JRhz-_a4AX9hrtFJ&_nc_ht=scontent-gru2-2.xx&oh=862d42a1e52703ea53331311c86d86fb&oe=5E975F05",
+  ),
+  new ChatModel(
+    name: "Eric Batista",
+    message: "Boa noite rapaz!! Bao",
+    time: "15:30",
+    avatarUrl: "https://scontent-gru2-2.xx.fbcdn.net/v/t1.0-9/53860949_1928452457264330_8480711902920441856_n.jpg?_nc_cat=106&_nc_sid=09cbfe&_nc_ohc=I__JRhz-_a4AX9hrtFJ&_nc_ht=scontent-gru2-2.xx&oh=862d42a1e52703ea53331311c86d86fb&oe=5E975F05",
+  ),
+  new ChatModel(
+    name: "Eric Batista",
+    message: "Boa noite rapaz!! Bao",
+    time: "15:30",
+    avatarUrl: "https://scontent-gru2-2.xx.fbcdn.net/v/t1.0-9/53860949_1928452457264330_8480711902920441856_n.jpg?_nc_cat=106&_nc_sid=09cbfe&_nc_ohc=I__JRhz-_a4AX9hrtFJ&_nc_ht=scontent-gru2-2.xx&oh=862d42a1e52703ea53331311c86d86fb&oe=5E975F05",
+  ),
+  new ChatModel(
+    name: "Eric Batista",
+    message: "Boa noite rapaz!! Bao",
+    time: "15:30",
+    avatarUrl: "https://scontent-gru2-2.xx.fbcdn.net/v/t1.0-9/53860949_1928452457264330_8480711902920441856_n.jpg?_nc_cat=106&_nc_sid=09cbfe&_nc_ohc=I__JRhz-_a4AX9hrtFJ&_nc_ht=scontent-gru2-2.xx&oh=862d42a1e52703ea53331311c86d86fb&oe=5E975F05",
+  )
+];
